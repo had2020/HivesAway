9a4fe23114d17882defafb3366d4c034db49f4e6
Hadrian Lazic <hadrian.lazic@gmail.com>
2024-08-24 15:56:27 -0700

    added icon, waring page

---
+++ Client/src/pages/waring.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Waring = (reason) => {
+  return (
+    <div>
+      <h1>I got to stop you their bro!</h1>
+      <h2>Please, {reason} </h2>
+    </div>
+  );
+};
+
+export default Waring;
